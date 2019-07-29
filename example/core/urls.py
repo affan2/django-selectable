d7924275cf650bdadf66c4314e562b8adbb28bad
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-07-29 13:32:43 +0400

    Remove example project

--- example/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from .views import formset, advanced, index
-
-urlpatterns = [
-    url(r'^formset/', formset, name='example-formset'),
-    url(r'^advanced/', advanced, name='example-advanced'),
-    url(r'^', index, name='example-index'),
-]
